992275584271e087d97a90fe9d0e7d288b9696bb
JesseKuntz <jessekuntz96@gmail.com>
2022-08-10 12:57:42 -0400

    feat: stake, unstake, and withdraw device transaction config adjustments

--- libs/ledger-live-common/src/families/near/deviceTransactionConfig.ts
+++ libs/ledger-live-common/src/families/near/deviceTransactionConfig.ts
@@ -8,18 +8,72 @@ function getDeviceTransactionConfig({
 }): Array<DeviceTransactionField> {
   const fields: Array<DeviceTransactionField> = [];
 
-  if (transaction.mode === "send") {
-    fields.push({
-      type: "text",
-      label: "Method",
-      value: "Transfer",
-    });
-  }
-
-  fields.push({
+  const confirmField = {
+    type: "text",
+    label: "Confirm",
+  } as DeviceTransactionField;
+  const amountField = {
     type: "amount",
     label: "Amount",
-  });
+  } as DeviceTransactionField;
+  const validatorField = {
+    type: "address",
+    label: "To",
+    address: transaction.recipient,
+  } as DeviceTransactionField;
+
+  switch (transaction.mode) {
+    case "stake":
+      fields.push({
+        ...confirmField,
+        value: "deposit_and_stake",
+      } as DeviceTransactionField);
+      fields.push({
+        type: "amount",
+        label: "Deposit",
+      });
+      fields.push(validatorField);
+      break;
+
+    case "unstake":
+      if (transaction.useAllAmount) {
+        fields.push({
+          ...confirmField,
+          value: "unstake_all",
+        } as DeviceTransactionField);
+      } else {
+        fields.push({
+          ...confirmField,
+          value: "unstake",
+        } as DeviceTransactionField);
+      }
+      fields.push(validatorField);
+      fields.push(amountField);
+      break;
+
+    case "withdraw":
+      if (transaction.useAllAmount) {
+        fields.push({
+          ...confirmField,
+          value: "withdraw_all",
+        } as DeviceTransactionField);
+      } else {
+        fields.push({
+          ...confirmField,
+          value: "withdraw",
+        } as DeviceTransactionField);
+      }
+      fields.push(validatorField);
+      fields.push(amountField);
+      break;
+
+    default:
+      fields.push({
+        ...confirmField,
+        value: "transfer",
+      } as DeviceTransactionField);
+      fields.push(amountField);
+  }
 
   return fields;
 }
